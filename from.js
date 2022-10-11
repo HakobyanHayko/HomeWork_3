02de97d86e1fed021e0c8546a01f2bca0e321b15
Hayk Hakobyan <115587947+HakobyanHayko@users.noreply.github.com>
2022-10-12 00:50:27 +0400

    Create from.js

---
+++ from.js
@@ -0,0 +1,12 @@
+//                       from
+//            task 44
+function getCloneFrom(array){
+  return Array.from(array)
+}
+// console.log(getCloneFrom([1,2,3,4,5]))
+
+//                    task  46
+  function turnArr(string){
+    return Array.from(string)
+  }
+// console.log(turnArr("Hayk"))
