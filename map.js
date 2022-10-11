b5652ed71d5fd8a0cfda426f71b8b3f6e9b4bb0c
Hayk Hakobyan <115587947+HakobyanHayko@users.noreply.github.com>
2022-10-12 00:45:58 +0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,11 @@
+//                  task 29
+function getCloneArr(array){
+   return array.map(item => item)
+}
+// console.log(getCloneArr([1,2,3,4]))
+
+//                   task 30
+function getOnlyNames(array){
+   return array.map(item => item.firstName)
+}
+// console.log(getOnlyNames(employees)) ;
